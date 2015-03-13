f4a1cc1fdfc105e56b02af5b57ba0960f0c229bc
Ludvig Liljenberg <ludfjig@hotmail.com>
2015-03-13 17:08:09 +0100

    test

---
+++ src/steganography/Magic.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package steganography;
+
+import java.awt.image.BufferedImage;
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.BitSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Ludvig
+ */
+public class Magic {
+
+    BufferedImage inputImage;
+    BufferedImage outputImage;
+    int[][] pixelColors;
+    BitSet bits;
+    String msg;
+    String bigString;
+    StringBuilder builder;
+    int temp = 0;
+
+    void doMagic(File file, String msg) {
+        this.msg = msg;
+        try {
+            inputImage = ImageIO.read(file);
+        } catch (IOException ex) {
+            Logger.getLogger(Magic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(), TYPE_INT_ARGB);
+        pixelColors = new int[inputImage.getWidth()][inputImage.getHeight()];
+
+        for (int i = 0; i < inputImage.getWidth(); i++) {
+            for (int j = 0; j < inputImage.getHeight(); j++) {
+                pixelColors[i][j] = getPixelAt(i, j);
+            }
+        }
+
+//        int color = pixelColors[9][0];
+//
+//        System.out.println(Integer.toBinaryString(color));
+//        char c = 'a';
+//        int ch = 97;
+//
+//        int colorAlpha = (color >> 24) & 0xFF;
+//        int colorRed = (color >> 16) & 0xFF;
+//        int colorGreen = (color >> 8) & 0xFF;
+//        int colorBlue = (color) & 0xFF;
+//        System.out.println("Alpha: \t" + Integer.toBinaryString(colorAlpha));
+//        System.out.println("Red: \t" + Integer.toBinaryString(colorRed));
+//        System.out.println("Green: \t" + Integer.toBinaryString(colorGreen));
+//        System.out.println("Blue: \t" + Integer.toBinaryString(colorBlue));
+        setNewPixels();
+
+    }
+
+    int getPixelAt(int x, int y) {
+        return inputImage.getRGB(x, y);
+    }
+
+    void setNewPixels() {
+        byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
+        builder = new StringBuilder();
+        for (byte b : bytes) {
+
+            String format = String.format("%%0%dd", 8);
+            String result = String.format(format, Integer.parseInt(Integer.toBinaryString(b)));
+
+            builder.append(result);
+        }
+        bigString = builder.toString();
+        System.out.println(bigString);
+
+        loop:
+        for (int i = 0; i < inputImage.getWidth(); i++) { // KAN VARA INVERTERAD? JAG VETINTE
+            for (int j = 0; j < inputImage.getHeight(); j++) {
+
+                if (temp <= bigString.length() - 1) {
+                    int c = bigString.charAt(temp) - 48;
+                    outputImage.setRGB(i, j, pixelColors[i][j] & c);
+                    temp++;
+                    System.out.println("Hej");
+                } else {
+                    break loop;
+                }
+            }
+        }
+        System.out.println("Done");
+    }
+
+    String getTextFromImage(BufferedImage image) {
+        StringBuilder builder2 = new StringBuilder();
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                builder2.append(image.getRGB(i, j) & 1);
+            }
+        }
+        String finishedString = builder2.toString();
+        char[] chars = new char[finishedString.length() / 8];
+        int temp2 = 0;
+
+        
+            for (int i = 0; i < finishedString.length(); i += 8) {
+                if (i + 8 <= finishedString.length()) {
+                    System.out.println(i + " " + (i+8));
+                    System.out.println(finishedString.substring(i, i+8));
+                    System.out.println(Integer.parseInt(finishedString.substring(i, i+8),2));
+                    System.out.println((char)Integer.parseInt(finishedString.substring(i, i+8),2));
+                    chars[temp2] = (char)Integer.parseInt(finishedString.substring(i, i+8),2);
+                }
+                temp2 ++;
+            }
+
+        
+        return new String(chars);
+        
+    }
+
+}
